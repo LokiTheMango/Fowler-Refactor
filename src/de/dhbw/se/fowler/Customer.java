8072dfaad2971ccf82340bff219d8a41f88db872
LokiTheMango <mirkomueller97@live.de>
2018-05-22 22:39:15 +0200

    extract total charge amount variable

--- src/de/dhbw/se/fowler/Customer.java
+++ src/de/dhbw/se/fowler/Customer.java
@@ -21,7 +21,6 @@ public class Customer {
     }
     
     public String statement() {
-        double totalAmount = 0;
         int frequentRenterPoints = 0;
         Enumeration enum_rentals = rentals.elements();	    
         String result = "Rental Record for " + this.getCustomername() + "\n";
@@ -37,11 +36,20 @@ public class Customer {
                 frequentRenterPoints++;
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(each.getCharge()) + "\n";
-            totalAmount += each.getCharge();
         }
         //add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        return result;
+    }
+    
+    private double getTotalCharge() {
+    	double result = 0;
+    	Enumeration enum_rentals = rentals.elements();
+        while (enum_rentals.hasMoreElements()) {
+            Rental each = (Rental) enum_rentals.nextElement();
+            result += each.getCharge();
+        }
         return result;
     }
 
