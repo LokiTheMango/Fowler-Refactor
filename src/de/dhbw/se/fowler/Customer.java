cbdba51a1a955d657f4a06f102c0c0dfc560290f
LokiTheMango <mirkomueller97@live.de>
2018-05-22 22:42:15 +0200

    extract getFrequentRenterPoints method

--- src/de/dhbw/se/fowler/Customer.java
+++ src/de/dhbw/se/fowler/Customer.java
@@ -30,10 +30,7 @@ public class Customer {
             Rental each = (Rental) enum_rentals.nextElement();
 
             // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) 
-                frequentRenterPoints++;
+            frequentRenterPoints += getFrequentRenterPoints(each);
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(each.getCharge()) + "\n";
         }
@@ -43,7 +40,14 @@ public class Customer {
         return result;
     }
     
-    private double getTotalCharge() {
+    private int getFrequentRenterPoints(final Rental each) {
+    	// add bonus for a two day new release rental
+    	if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+    		return 2;
+    	return 1;
+	}
+
+	private double getTotalCharge() {
     	double result = 0;
     	Enumeration enum_rentals = rentals.elements();
         while (enum_rentals.hasMoreElements()) {
